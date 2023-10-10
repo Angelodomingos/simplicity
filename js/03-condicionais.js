64b48d0507e143dd885546956e42f915bbd706e5
ÂngeloDomingos <domingosangelo731@gmail.com>
2023-10-10 15:15:51 -0300

    Js

---
+++ js/03-condicionais.js
@@ -0,0 +1,24 @@
+console.log("testo")
+
+
+/*Estruturas de controle Condicional
+comando que pewrmitem analisar uma ou ms condições. e apartir disso (verdadeiro ou falso),
+o programa pode excutar ações diferentes. 
+
+
+
+
+comando mais comuns : if(se), else(senão se)
+
+*/
+
+
+let usario = "Angelo"
+let idade = 25;
+
+//verificar a idade de determinar se o menor ou maior //
+is(idade >= 18){
+    console.log(`${usario} é maior de idade`);
+} else {
+    console.log(`${usario} é menor de idade`);
+}
